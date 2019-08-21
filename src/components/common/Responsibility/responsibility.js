d1573645d21dac8ece99ef9ee8bdca3ef0f5318a
Michal Pawlaczyk <michalm441@gmail.com>
2019-08-21 22:46:11 +0200

    fetch: Added custom responsibility breakpoints

---
+++ src/components/common/Responsibility/responsibility.js
@@ -0,0 +1,9 @@
+import { generateMedia } from "styled-media-query"
+
+export const customMedia = generateMedia({
+  huge: "1440px",
+  large: "1170px",
+  big: "980px",
+  medium: "768px",
+  small: "450px",
+})
