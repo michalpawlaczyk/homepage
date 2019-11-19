600ca853e6f796b710d673f184b1b17cd39510cb
Michal Pawlaczyk <michalm441@gmail.com>
2019-11-19 18:07:18 +0100

    Added font sizes in Theme, Added prettier & eslint

--- src/Theme/mainTheme.js
+++ src/Theme/mainTheme.js
@@ -1,6 +1,16 @@
-export const theme = {
-  huge: "1440px",
-  large: "1170px",
-  medium: "780px",
-  small: "450px",
-}
+const theme = {
+  huge: '1440px',
+  large: '1170px',
+  medium: '780px',
+  small: '450px',
+  font: {
+    xs: '1.3rem',
+    s: '1.6rem',
+    m: '2.5rem',
+    l: '3.3rem',
+    xl: '4rem',
+    xxl: '4.5rem',
+  },
+};
+
+export default theme;
