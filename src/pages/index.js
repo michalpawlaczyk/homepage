a2ca8d006ae4c1e54b2393a479f6c74bdbd74501
Michal Pawlaczyk <michalm441@gmail.com>
2020-02-10 16:40:03 +0100

    fetch: Created preliminary version of the main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,91 @@
-import React from "react"
-import { Welcome } from "../components/Welcome/index"
-import { Skills } from "../components/Skills/index"
-import { Projects } from "../components/Projects/index"
-import { ProjectsDescription } from "../components/Projects/index"
-import { Contact } from "../components/Contact/index"
-import { graphql } from "gatsby"
-import GlobalStyle from "../Theme/GlobalStyle"
+import React from 'react';
+import { graphql } from 'gatsby';
+import styled from 'styled-components';
+import GlobalStyle from 'Theme/GlobalStyle';
+import theme from 'Theme/mainTheme';
+import MainView from 'components/organisms/MainView/MainView';
+import image from 'static/webdev3.svg';
+import SkillsView from 'components/organisms/SkillsView/SkillsView';
+import ContactView from 'components/organisms/ContactView/ContactView';
+import GitHubProjectCard from 'components/molecules/GitHubProjectCard/GitHubProjectCard';
+import Heading from 'components/atoms/Heading/Heading';
+
+const StyledProjectsWrapper = styled.section`
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  margin: 4vh;
+  grid-gap: 30px;
+  justify-items: center;
+  @media (min-width: ${theme.medium}) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media (min-width: ${theme.large}) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+`;
 
 export default ({ data }) => {
   return (
     <main>
       <GlobalStyle />
-      <Welcome />
-      <Skills id="skills" />
-      <Projects id="projects">
+      <MainView
+        title={`Hey there! 👋 I’m Michał and I’m aspiring to frontend developer`}
+        image={image}
+        imageAlt={'Web developer.'}
+      />
+      <SkillsView
+        mainTitle="Skills"
+        listData={[
+          {
+            list: {
+              listTitle: 'Working knowlage',
+              listItems: [
+                { value: 'HTML5' },
+                { value: 'CSS' },
+                { value: 'Figma' },
+                { value: 'JavaScript' },
+                { value: 'GIT' },
+              ],
+            },
+          },
+        ]}
+      />
+      <SkillsView
+        listData={[
+          {
+            list: {
+              listTitle: 'Know something about',
+              listItems: [
+                { value: 'Progressive web apps' },
+                { value: 'NPM' },
+                { value: 'Gulp' },
+                { value: 'React JS' },
+              ],
+            },
+          },
+        ]}
+      />
+      <SkillsView
+        listData={[
+          {
+            list: {
+              listTitle: 'Want to learn',
+              listItems: [{ value: 'React native' }, { value: 'Node.js' }],
+            },
+          },
+        ]}
+      />
+      <Heading as="h2" topLine>
+        Projects
+      </Heading>
+      <StyledProjectsWrapper>
         {data ? (
           data.github.repositoryOwner.repositories.nodes.map(element => (
-            <ProjectsDescription
+            <GitHubProjectCard
               key={element.name}
               name={element.name}
               description={element.description}
-              url={element.url}
+              sourceUrl={element.url}
             />
           ))
         ) : (
@@ -31,17 +96,23 @@ export default ({ data }) => {
             </span>
           </p>
         )}
-      </Projects>
-      <Contact id="contact" />
+      </StyledProjectsWrapper>
+      <ContactView
+        title="Contact me 😄"
+        paragraph="I’m open for new opportunities. If you have one for me we should talk!"
+        linkedInLink="https://pl.linkedin.com/"
+        githubLink="https://github.com/michalpawlaczyk"
+        email="michallpawlaczyk@gmail.com"
+      />
     </main>
-  )
-}
+  );
+};
 
 export const query = graphql`
   query {
     github {
       repositoryOwner(login: "michalpawlaczyk") {
-        repositories(first: 10) {
+        repositories(first: 9) {
           nodes {
             url
             description
@@ -51,4 +122,4 @@ export const query = graphql`
       }
     }
   }
-`
+`;
